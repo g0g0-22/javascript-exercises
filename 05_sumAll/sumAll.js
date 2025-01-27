16e908d4aaed5a390e795e6a991875f323bf10c0
g0g0-22 <gman89959@gmail.com>
2025-01-27 12:47:32 +0100

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
 
+    if(a < 0 || b < 0 || Number.isInteger(a) === false || Number.isInteger(b) === false)
+        return 'ERROR';
+
+    for(let i = Math.min(a,b); i <= Math.max(a,b); i++){
+        sum += i;
+    }
+    return sum;
+    
 };
 
 // Do not edit below this line
