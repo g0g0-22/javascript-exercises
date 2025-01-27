3a86ee8deae3c78674bb0e4959326290d4d34e8f
g0g0-22 <gman89959@gmail.com>
2025-01-27 12:38:16 +0100

    simplified code / solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,5 @@
-const removeFromArray = function() {
-    const array = arguments[0];
-    for(let i = 1; i < arguments.length; i++) {
-        const index = array.indexOf(arguments[i]);
-        if(index !== -1) {
-            array.splice(index, 1);
-            i--;
-        }
-    }
-    return array;
+const removeFromArray = function(array, ...args) {
+    return array.filter(val => !args.includes(val))
 };
 
-// Do not edit below this line
 module.exports = removeFromArray;
